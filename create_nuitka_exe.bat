d4dfb9e10fa2d3fcc975f0fec3ecde518b4ad19e
Paul-Hi <32524715+Paul-Hi@users.noreply.github.com>
2022-06-28 21:37:48 +0200

    fix missing ressource file in onefile executable

--- create_nuitka_exe.bat
+++ create_nuitka_exe.bat
@@ -7,4 +7,4 @@ pip install nuitka==0.8.4
 pip install ordered-set
 pip install zstandard
 
-py -m nuitka --include-data-file=res/default.css=res/default.css --include-data-file=res/default.cfg=res/default.cfg --include-data-file=res/functions.js=res/functions.js --include-data-file=testdata/issue_portal_test_data.html=testdata/issue_portal_test_data.html --include-data-file=res/logo_base64.txt=res/logo_base64.txt --onefile il_conv.py
+py -m nuitka --include-data-file=res/default.css=res/default.css --include-data-file=res/default.cfg=res/default.cfg --include-data-file=res/functions.js=res/functions.js --include-data-file=testdata/issue_portal_test_data.html=testdata/issue_portal_test_data.html --include-data-file=res/logo_base64.txt=res/logo_base64.txt --include-data-file=res/logo.png=res/logo.png --onefile il_conv.py
